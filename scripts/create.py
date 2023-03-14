fd9d8b92eb97d9fdac7470324c3336fc3df0bd6b
jarbasai <jarbasai@mailfence.com>
2023-03-14 20:03:11 +0000

    :tada:

---
+++ scripts/create.py
@@ -0,0 +1,37 @@
+import json
+from os import makedirs, listdir
+from os.path import dirname, isfile
+from time import sleep
+from ovos_plugin_manager.tts import load_tts_plugin
+
+WW_CFGS = f"{dirname(dirname(__file__))}/ww_configs"
+VOICES_BASE = f"{dirname(dirname(__file__))}/tts_voices"
+OUTPUT_BASE = f"{dirname(dirname(__file__))}/synth_data"
+
+for cfg in listdir(WW_CFGS):
+    with open(f"{WW_CFGS}/{cfg}") as f:
+        CONF = json.load(f)
+    
+    LANG = CONF.get("lang", "en-us")
+    WW = CONF["name"]
+    VOICE_IDS = CONF.get("tts_voices") or []
+
+    VOICES_FOLDER = f"{VOICES_BASE}/{LANG}"
+    OUTPUT_FOLDER = f"{OUTPUT_BASE}/{WW.lower().replace(' ', '_')}"
+    makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    for voice in VOICE_IDS:
+        cfg = f"{VOICES_FOLDER}/{voice}"
+        if not isfile(cfg):
+            continue
+        with open(cfg) as f:
+            cfg = json.load(f)
+        m = cfg.pop("module")
+
+        engine = load_tts_plugin(m)(config=cfg)
+        wav_file = f"{OUTPUT_FOLDER}/{voice.replace('.json', f'.{engine.audio_ext}')}"
+        if isfile(wav_file):
+            continue
+        engine.get_tts(WW, wav_file)
+        if "server" in voice:
+            sleep(1)  # do not overload public servers
---
+++ scripts/scan_voices.py
@@ -0,0 +1,29 @@
+from ovos_plugin_manager.tts import get_tts_lang_configs
+from os.path import dirname
+from os import makedirs
+import json
+
+LANG = "en-us"
+
+VOICES_FOLDER = f"{dirname(dirname(__file__))}/tts_voices/{LANG}"
+makedirs(VOICES_FOLDER, exist_ok=True)
+
+voice_ids = []
+
+for plug, voices in get_tts_lang_configs(LANG, include_dialects=True).items():
+    for voice in voices:
+        spkr = voice.get("speaker") or voice.get("lang") or voice.get("gender") or "default"
+        name = voice.get("voice") or voice.get("model") or voice.get("lang") or LANG
+        voiceid = f"{plug}_{name}_{spkr}.json".replace("/", "_")
+        voice_ids.append(voiceid)
+        noise = ["meta", "priority", "display_name", "offline", "gender"]
+        for k in noise:
+            if k in voice:
+                voice.pop(k)
+        voice["module"] = plug
+        with open(f"{VOICES_FOLDER}/{voiceid}", "w") as f:
+            json.dump(voice, f, indent=4)
+
+from pprint import pprint
+pprint(voice_ids)
+
