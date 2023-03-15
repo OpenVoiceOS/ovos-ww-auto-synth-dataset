de5d5eff39e76c41e6b9fa8ba3609d2cd5ad35a7
jarbasai <jarbasai@mailfence.com>
2023-03-15 13:52:29 +0000

    convert mp3 to wav

--- scripts/convert.py
+++ scripts/convert.py
@@ -3,25 +3,22 @@ from os import listdir, makedirs, walk, rename
 from os.path import join, isdir, isfile, dirname
 import json
 
-WW_CFGS = f"auto_synth/ww_configs"
-SAMPLES_BASE = f"auto_synth/synth_data"
-OUTPUT_BASE = f"auto_synth/synth_data_converted"
+WW_CFGS = f"{dirname(dirname(__file__))}/ww_configs"
+SAMPLES_BASE = f"{dirname(dirname(__file__))}/synth_data"
+OUTPUT_BASE = SAMPLES_BASE
+
+
+exts = ["mp3", "mp4"]
 
 
 # this will convert all files to the correct format
-def convert_dir(SOURCE_DIR, DEST_DIR, overwrite=False):
+def convert_dir(SOURCE_DIR, DEST_DIR=None, overwrite=True):
+    DEST_DIR = DEST_DIR or SOURCE_DIR
     if not isdir(DEST_DIR):
         makedirs(DEST_DIR)
 
-    exts = ["mp3", "wav", "mp4"]
     for wav in listdir(SOURCE_DIR):
         if wav.split(".")[-1] not in exts:
-            # non audio file, skip
-            if isdir(join(SOURCE_DIR, wav)):
-                # TODO recursive
-                print("skipping directory", wav)
-            else:
-                print("unrecognized format, skipping", wav)
             continue
         print("converting ", wav)
         if wav.endswith(".wav"):
@@ -41,13 +38,8 @@ def convert_dir(SOURCE_DIR, DEST_DIR, overwrite=False):
 for cfg in listdir(WW_CFGS):
     with open(f"{WW_CFGS}/{cfg}") as f:
         CONF = json.load(f)
-    ww = cfg.replace(".json", ".wav")
+    ww = cfg.replace(".json", "")
     src = f"{SAMPLES_BASE}/{ww}"
-    if not isfile(src):
+    if not isdir(src):
         continue
-    dst = f"{OUTPUT_BASE}/{ww}"
-
-    for root, dirs, files in walk(src, topdown=False):
-        path = root
-        dest = path.replace(src, dst)
-        convert_dir(root, dest)
+    convert_dir(src)
