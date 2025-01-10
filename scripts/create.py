ef71672b70e5f340485380c6b0482f2a71f6cdf9
miro <jarbasai@mailfence.com>
2025-01-10 21:20:26 +0000

    more voices

--- scripts/create.py
+++ scripts/create.py
@@ -11,6 +11,8 @@ OUTPUT_BASE = f"{dirname(dirname(__file__))}/synth_data"
 engines = {}
 
 for cfg in listdir(WW_CFGS):
+    if not cfg.endswith(".json"):
+        continue
     with open(f"{WW_CFGS}/{cfg}") as f:
         CONF = json.load(f)
     
@@ -33,7 +35,17 @@ for cfg in listdir(WW_CFGS):
         if m in engines:
             engine = engines[m]
         else:
-            engine = engines[m] = load_tts_plugin(m)(config=cfg)
+            clazz = load_tts_plugin(m)
+
+            if clazz is None:
+                print(f"plugin not installed: {m}")
+                continue
+            try:
+                engine = engines[m] = clazz(config=cfg)
+            except:
+                print("failed to load plugin", m)
+                continue
+
         wav_file = f"{OUTPUT_FOLDER}/{voice.replace('.json', f'.{engine.audio_ext}')}"
         if isfile(wav_file) or isfile(wav_file + ".wav"):  # handle converted mp3 files
             continue
@@ -43,6 +55,11 @@ for cfg in listdir(WW_CFGS):
             kwargs["speaker"] = cfg["speaker"]
         if "voice" in cfg:
             kwargs["voice"] = cfg["voice"]
-        engine.get_tts(WW, wav_file, **kwargs)
+
+        try:
+            engine.get_tts(WW, wav_file, **kwargs)
+        except:
+            print(f"synth failed! {wav_file}")
+            continue
         if "server" in voice:
             sleep(1)  # do not overload public servers
